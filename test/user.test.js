84137b02103357fce80a3bf05c4c9a8251f4cb21
mazufik <taufikprincecode10@gmail.com>
2023-07-17 15:08:07 +0900

    Create Unit Test for Logout User API

--- test/user.test.js
+++ test/user.test.js
@@ -233,3 +233,34 @@ describe("PATCH /api/users/current", () => {
     expect(result.status).toBe(401);
   });
 });
+
+describe("DELETE /api/users/logout", () => {
+  beforeEach(async () => {
+    await createTestUser();
+  });
+
+  afterEach(async () => {
+    await removeTestUser();
+  });
+
+  it("should can logout", async () => {
+    const result = await supertest(web)
+      .delete("/api/users/logout")
+      .set("Authorization", "test");
+
+    expect(result.status).toBe(200);
+    expect(result.body.data).toBe("OK");
+
+    const user = await getTestUser();
+    expect(user.token).toBeNull();
+  });
+
+  // token salah
+  it("should reject logout if token is invalid", async () => {
+    const result = await supertest(web)
+      .delete("/api/users/logout")
+      .set("Authorization", "salah");
+
+    expect(result.status).toBe(401);
+  });
+});
