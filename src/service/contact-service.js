06e52a4e35cb87220740d15b7a82ab5fbb6d69f2
mazufik <taufikprincecode10@gmail.com>
2023-07-19 14:19:17 +0900

    Create Unit Test for Remove Contacts API

--- src/service/contact-service.js
+++ src/service/contact-service.js
@@ -86,14 +86,14 @@ const update = async (user, request) => {
 const remove = async (user, contactId) => {
   contactId = validate(getContactValidation, contactId);
 
-  const { count } = await prismaClient.contact.count({
+  const totalInDatabase = await prismaClient.contact.count({
     where: {
       username: user.username,
       id: contactId,
     },
   });
 
-  if (!count) {
+  if (totalInDatabase !== 1) {
     throw new ResponseError(404, "contact is not found");
   }
 
